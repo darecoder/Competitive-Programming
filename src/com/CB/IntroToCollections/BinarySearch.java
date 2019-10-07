6108e8054348ce674d1aad99eeaf9c915c8247ca
ekta1999 <ektamishra1999@gmail.com>
2019-10-07 10:19:50 +0530

    Initial Commit

---
+++ src/com/CB/IntroToCollections/BinarySearch.java
@@ -0,0 +1,4 @@
+package com.CB.IntroToCollections;
+
+public class BinarySearch {
+}
---
+++ src/com/CB/IntroToCollections/Sort.java
@@ -0,0 +1,20 @@
+package com.CB.IntroToCollections;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Sort {
+    public static void main(String[] args) {
+        int[] nums = {1, 4, 7, 2, 9, 0, 3, 6, 8, 5};   //Original Array
+
+        Arrays.sort(nums);  //In-built Sort funcction
+
+        System.out.println(Arrays.toString(nums));  //To print the array
+
+        Integer[] arr = {1, 4, 7, 2, 9, 0, 3, 6, 8, 5};
+
+        Arrays.sort(arr, Collections.reverseOrder());
+
+        System.out.println(Arrays.toString(arr));  //To print the array in reverse order
+    }
+}
