c29d662febb0365638404528464ea82213f760e5
ekta1999 <ektamishra1999@gmail.com>
2019-10-13 21:56:47 +0530

    Bitmasking'

---
+++ src/com/CB/Bitmasking/CountBits.java
@@ -0,0 +1,7 @@
+package com.CB.Bitmasking;
+
+public class CountBits {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/com/CB/Bitmasking/UniqueNumberIII.java
@@ -0,0 +1,15 @@
+package com.CB.Bitmasking;
+
+import java.util.Scanner;
+
+public class UniqueNumberIII {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int[] nums = new int[n];
+        for (int i = 0; i < n; i++) {
+            nums[i] = s.nextInt();
+        }
+        System.out.println();
+    }
+}
