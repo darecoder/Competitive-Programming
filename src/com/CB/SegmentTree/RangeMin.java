1fd5e542fbe018e6dcc6de102a493532c87093f0
ekta1999 <ektamishra1999@gmail.com>
2019-11-06 09:57:57 +0530

    add

--- src/com/CB/SegmentTree/RangeMin.java
+++ src/com/CB/SegmentTree/RangeMin.java
@@ -5,6 +5,10 @@ import java.util.Scanner;
 public class RangeMin {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
 
+        for (int i = 0; i < n; i++) {
+
+        }
     }
 }
