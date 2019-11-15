7bbf64762bac4b2955271f397ef02af7bb449030
ekta1999 <ektamishra1999@gmail.com>
2019-11-15 22:24:47 +0530

    Mathematics

--- src/com/CB/Mathematics/MatrixExponentiation.java
+++ src/com/CB/Mathematics/MatrixExponentiation.java
@@ -1,7 +1,10 @@
 package com.CB.Mathematics;
 
+import java.util.Scanner;
+
 public class MatrixExponentiation {
     public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
 
     }
 }
