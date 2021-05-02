2b0702d013d72346df25b1d762804c51016f1836
darecoder <ektamishra1999@gmail.com>
2021-05-02 17:05:47 +0530

    Dynamic Programming

--- src/com/company/DynamicProgramming/LongIncSubseq.java
+++ src/com/company/DynamicProgramming/LongIncSubseq.java
@@ -1,6 +1,29 @@
 package com.company.DynamicProgramming;
 
+import java.util.Arrays;
+
 public class LongIncSubseq {
+    public static void main(String[] args) {
+        int[] nums = {10, 22, 9, 33, 21, 50, 41, 60, 80};
+        System.out.println(longIncSubseq(nums));
+    }
+    public static int longIncSubseq(int[] nums) {
+        int[] mem = new int[nums.length];
+        int i, j, max = 0;
 
-
+        Arrays.fill(mem, 1);
+        for (i = 1; i < nums.length; i++) {
+            for (j = 0; j < i; j++) {
+                if (nums[i] > nums[j] && mem[i] < mem[j] + 1) {
+                    mem[i] = mem[j] + 1;
+                }
+            }
+        }
+        for (i = 0; i < nums.length; i++) {
+            if (max < mem[i]) {
+                max = mem[i];
+            }
+        }
+        return max;
+    }
 }
